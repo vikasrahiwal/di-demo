9d2adcb096248f4eb2738b4bd2d172cfca41b3a8
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-08 11:18:12 +0530

    Application Context test code - By Vikas

--- src/main/java/spring/vks/didemo/controllers/MyController.java
+++ src/main/java/spring/vks/didemo/controllers/MyController.java
@@ -1,4 +1,16 @@
 package spring.vks.didemo.controllers;
 
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Vikas Singh
+ * @Date Wed 08, May 2019
+ */
+@Controller
 public class MyController {
+
+    public String contextInit(){
+        System.out.println("Context Init");
+        return "foo";
+    }
 }
