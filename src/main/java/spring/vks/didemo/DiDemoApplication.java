53bea83a6625f2a54743f2b669a106c6947545f7
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-08 11:18:38 +0530

    Application Context test code - By Vikas

--- src/main/java/spring/vks/didemo/DiDemoApplication.java
+++ src/main/java/spring/vks/didemo/DiDemoApplication.java
@@ -2,12 +2,17 @@ package spring.vks.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import spring.vks.didemo.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiDemoApplication.class, args);
+
+        ApplicationContext actx = SpringApplication.run(DiDemoApplication.class, args);
+        MyController myController = (MyController) actx.getBean("myController");
+        myController.contextInit();
     }
 
 }
