db0943015e404ac915ee216b41b0c98005ca50c0
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-09 12:26:08 +0530

    Constructor Based DI demo - By Vikas

--- src/main/java/spring/vks/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/spring/vks/didemo/controllers/ConstructorInjectedController.java
@@ -1,15 +1,24 @@
 package spring.vks.didemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import spring.vks.didemo.services.GreetingService;
 
 /**
  * @author Vikas Singh
  * @Date Wed 08, May 2019
  */
+@Controller
 public class ConstructorInjectedController {
 
     private GreetingService greetingService;
 
+    /* Do not need to used @Autowired here
+     * because around Spring 4.2, they enable automatic
+     * wiring of constructor based DI. But it will not generate any error
+     * if we use @Autowired over here
+     */
+    @Autowired
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
