83c65038d2c430f2ad83b753d609d52848bdbc66
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-09 11:22:58 +0530

    How to inject Contructor Based DI Demo - By Vikas

---
+++ src/test/java/spring/vks/didemo/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,26 @@
+package spring.vks.didemo.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import spring.vks.didemo.services.GreetingServiceImpl;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Vikas Singh
+ * @Date Thu 09, May 2019
+ */
+public class ConstructorInjectedControllerTest {
+
+    private ConstructorInjectedController constructorInjectedController;
+
+    @Before
+    public void setUp() throws Exception{
+        this.constructorInjectedController = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    public void testGreeting() throws Exception{
+        assertEquals(GreetingServiceImpl.HELLO_TECH, constructorInjectedController.sayHello());
+    }
+}
