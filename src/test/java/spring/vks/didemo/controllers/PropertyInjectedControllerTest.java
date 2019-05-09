7cc18aff41f006d2bc7e2d30074d820b83e23eb5
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-09 11:24:27 +0530

    How to inject Property Based DI Demo - By Vikas

---
+++ src/test/java/spring/vks/didemo/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,25 @@
+package spring.vks.didemo.controllers;
+import org.junit.Test;
+import spring.vks.didemo.services.GreetingServiceImpl;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+/**
+ * @author Vikas Singh
+ * @Date Wed 08, May 2019
+ */
+public class PropertyInjectedControllerTest {
+
+    private PropertyInjectedController propertyInjectedController;
+
+    @Before
+    public void setUp() throws Exception{
+        this.propertyInjectedController = new PropertyInjectedController();
+        this.propertyInjectedController.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    public void testGreeting() throws Exception{
+        assertEquals(GreetingServiceImpl.HELLO_TECH, propertyInjectedController.sayHello());
+    }
+}
