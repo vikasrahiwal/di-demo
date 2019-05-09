caaa86a0294b7d62c003ab9a52a9482525a42be9
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-09 12:27:05 +0530

    Setter Based DI demo - By Vikas

--- src/main/java/spring/vks/didemo/controllers/SetterInjectedController.java
+++ src/main/java/spring/vks/didemo/controllers/SetterInjectedController.java
@@ -1,11 +1,14 @@
 package spring.vks.didemo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import spring.vks.didemo.services.GreetingService;
 
 /**
  * @author Vikas Singh
  * @Date Wed 08, May 2019
  */
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
@@ -15,6 +18,7 @@ public class SetterInjectedController {
         return greetingService.sayGreeting();
     }
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
