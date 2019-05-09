e6d6025da70ad71f5d54a2b00f5de3743986f8be
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-09 11:21:39 +0530

    Contructor Based DI demmo - By Vikas

---
+++ src/main/java/spring/vks/didemo/controllers/ConstructorInjectedController.java
@@ -0,0 +1,20 @@
+package spring.vks.didemo.controllers;
+
+import spring.vks.didemo.services.GreetingService;
+
+/**
+ * @author Vikas Singh
+ * @Date Wed 08, May 2019
+ */
+public class ConstructorInjectedController {
+
+    private GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    String sayHello(){
+        return  greetingService.sayGreeting();
+    }
+}
