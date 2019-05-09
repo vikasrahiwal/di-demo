18ef3a6b48a3f5d83605cc2862c8fd16c8d7e9ca
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-09 11:23:51 +0530

    Property Based DI demo - By Vikas

---
+++ src/main/java/spring/vks/didemo/controllers/PropertyInjectedController.java
@@ -0,0 +1,18 @@
+package spring.vks.didemo.controllers;
+
+
+import spring.vks.didemo.services.GreetingServiceImpl;
+
+/**
+ * @author Vikas Singh
+ * @Date Wed 08, May 2019
+ */
+public class PropertyInjectedController {
+
+    public GreetingServiceImpl greetingService;
+
+    String sayHello(){
+        return greetingService.sayGreeting();
+    }
+
+}
