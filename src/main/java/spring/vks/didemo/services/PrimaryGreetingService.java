59e7b9b04c2959bb8e5fdf6cf794c8b2a16b5d13
Vikas Singh <vikasrahiwal@gmail.com>
2019-05-20 12:09:28 +0530

    By Vikas

--- src/main/java/spring/vks/didemo/services/PrimaryGreetingService.java
+++ src/main/java/spring/vks/didemo/services/PrimaryGreetingService.java
@@ -10,7 +10,19 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Primary
-@Profile("en")
+/*
+01. @Profile can takes an array as an arg. for different type of Profiles.
+02. "default" Profile mark this bean as DEFAULT BEAN so whenever there will be no active profile
+then this profile will be invoke by default.
+Note: To check the active profile, look for "spring.profiles.active" property in application.properties file. Its value
+should be blank to invoke the default profile bean. Otherwise the mentioned profile will be invoke.
+example:
+01. Invoke default profile
+    spring.profiles.active=
+02. Invoke other profile (invoking German profile)
+    spring.profiles.active=eg
+ */
+@Profile({"en","default"})
 public class PrimaryGreetingService implements GreetingService {
 
     @Override
